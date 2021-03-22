263626e1f10b7f0bb3ea285d9f22797d160ff5bf
Jocelynn Hartwig <jhartwig@apterainc.com>
2021-03-22 13:52:57 -0400

    feat: alt text generator api call

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+
+  const [altText, setAltText] = useState();
+
+  const imageSrc = "https://www.sciencenews.org/wp-content/uploads/2019/07/072319_ee_cat-allergy_feat.jpg"
+
+  useEffect(() => {
+    const body = {
+      url: imageSrc,
+      language: "en"
+    };
+
+    fetch('https://alt-text-generator.azurewebsites.net:443/api/Alt-Text-Generator/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=gRkN8GmmAPVncl6kxRu_RhpoYF2wKDLKqRM51ULixQY',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body)
+      })
+      .then(response => response.json())
+      .then(response => setAltText(response[0].text))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {
+          altText &&
+          <img src={imageSrc} alt={altText} />
+        }
       </header>
     </div>
   );
