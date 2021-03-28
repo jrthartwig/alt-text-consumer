afc51b669d336e849de75a9ea6e819e461af887c
Jocelynn <55405874+jrthartwig@users.noreply.github.com>
2021-03-28 15:09:27 -0400

    Update api-endpoints.js
    new logic app url

--- src/api-endpoints.js
+++ src/api-endpoints.js
@@ -1,2 +1,2 @@
-export const APP_ENDPOINT = 'https://alt-text-generator.azurewebsites.net:443/api/Alt-Text-Generator/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=gRkN8GmmAPVncl6kxRu_RhpoYF2wKDLKqRM51ULixQY';
+export const APP_ENDPOINT = 'https://alt-text-generator.azurewebsites.net:443/api/alt-text-generator-v2/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=7zMk99_noDbj5x5fHj2ICzse4iW1hILC2ovPD71CMVQ';
 export const APP_IMAGE_ENDPOINT = 'https://northcentralus.api.cognitive.microsoft.com/vision/v3.0/describe?maxCandidates=1&language=en';
