485c1cd5499a8145c3ddd884fecd9a96bf010423
Benjamin Bakle <bbakle@apterainc.com>
2021-03-27 21:56:25 -0400

    feat: add font awesome icons

---
+++ src/Icons/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileAlt, faGlobe, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+
+export const Globe = () => <FontAwesomeIcon icon={faGlobe} />
+export const File = () => <FontAwesomeIcon icon={faFileAlt} />
+export const Sync = () => <FontAwesomeIcon icon={faSyncAlt} />
